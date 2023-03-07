e432d80067142adb2d48cdf5c8edffc30337d540
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-03-07 20:48:43 +0530

    Change to npm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Layout from "./Components/Layout/Layout";
-import { FaBeer } from "@react-icons/all-files";
 
 
 function App() {
-
     return <Layout />;
-
 };
 
 export default App;
