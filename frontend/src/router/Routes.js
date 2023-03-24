a96ca7337c004371fc86e99c2e30b90505618335
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-03-24 11:54:48 +0530

    UI completed

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -2,10 +2,31 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 
+import OMHome from '../pages/OMdashboard/OMHome'
+
+import Schedule from '../pages/schedule/Schedule'
+import EditInstructor from '../pages/schedule/scheduleEdit/EditInstructor'
+import EditTimeSlot from '../pages/schedule/scheduleEdit/EditTimeSlot'
+import ScheduleTable from '../pages/schedule/ScheduleTable'
+import InstructorConfirmOM from '../pages/schedule/InstructorConfirmOM'
+
+
+
+
 const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<Navigate to='/index' />} />
+
+            {/* operation manager homepage */}
+            <Route path="/home" element={<OMHome />} />
+
+            {/* Schedule pages routing */}
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/changetimeslot" element={<EditTimeSlot />} />
+            <Route path="/changeinstrutor" element={<EditInstructor />} />
+            <Route path="/trainings" element={<ScheduleTable />} />
+            <Route path="/confirmation" element={<InstructorConfirmOM />} />
 
         </Routes>
     )
