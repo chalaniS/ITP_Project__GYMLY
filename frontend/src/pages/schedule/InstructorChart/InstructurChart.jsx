66f682d491458fb191e3fbad6ed334ace6e083fe
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-03-24 17:00:09 +0530

    UI completed

---
+++ frontend/src/pages/schedule/InstructorChart/InstructurChart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InstrIchartuctorChart from './Ichart';
+
+function InstructorChart() {
+    return (
+        <div>
+            <h1>My Page</h1>
+            <InstructorChart />
+        </div>
+    );
+}
+
+export default InstructorChart;
