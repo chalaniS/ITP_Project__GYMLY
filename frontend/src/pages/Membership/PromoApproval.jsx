977c56bdbadbbf30d144ed56b9c1f1364d0fd161
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-04-18 09:49:50 +0530

    Delete frontend/src/pages/Membership directory

--- frontend/src/pages/Membership/PromoApproval.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PromoApproval() {
-    return (
-        <div>PromoApproval</div>
-    )
-}
-
-export default PromoApproval
--- frontend/src/pages/Membership/PromoPackages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PromoPackages() {
-    return (
-        <div>PromoPackages</div>
-    )
-}
-
-export default PromoPackages
