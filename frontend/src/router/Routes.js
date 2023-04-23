2cb1259fd548085cc2da9d35b68407ed252cf91b
Sithum Asitha <123850774+SithumAsitha@users.noreply.github.com>
2023-04-23 14:44:15 +0530

    Update Routes.js

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -14,6 +14,18 @@ import Editprofile from '../pages/Users/Editprofile'
 import ManagerDashboard from '../pages/Users/UserManager/ManagerDashboard'
 import ManagmentLogin from '../pages/Users/UserManager/ManagementLoging'
 
+{/*Customer Affairs Management*/}
+import InstructorFeedbackform from '../pages/CustomerAffairs/InstructorFeedbackform'
+import ServiceFeedbackform from '../pages/CustomerAffairs/ServiceFeedbackform'
+import ServiceFeedbackApproval from '../pages/CustomerAffairs/ServiceFeedbackApproval'
+import InstructorFeedbackApproval from '../pages/CustomerAffairs/InstructorFeedbackApproval'
+import FeedbackOptions from '../pages/CustomerAffairs/FeedbackOptions'
+import SearchInstructor from '../pages/CustomerAffairs/SearchInstructor'
+import InstructorProfile from '../pages/CustomerAffairs/InstructorProfile'
+import AboutUs from '../pages/CustomerAffairs/AboutUs'
+import ContactUs from '../pages/CustomerAffairs/ContactUs'
+import EditInstructorFeedback from '../pages/CustomerAffairs/EditInstructorFeedback'
+
 const Router = () => {
     return (
         <Routes>
@@ -35,7 +47,18 @@ const Router = () => {
             <Route path="/ManagerDashboard" element={<ManagerDashboard />} />
             <Route path="/ManagmentLogin" element={<ManagmentLogin />} />
             {/*User Management*/}
-
+            
+            {/* Customer Affairs pages routing */}
+             <Route path="/InstructorFeedbackForm" element={<InstructorFeedbackform />} />
+             <Route path="/ServiceFeedbackform" element={<ServiceFeedbackform />} />
+              <Route path="/ServiceFeedbackApproval" element={<ServiceFeedbackApproval />} />
+              <Route path="/InstructorFeedbackApproval" element={<InstructorFeedbackApproval />} />
+              <Route path="/SearchInstructor" element={<SearchInstructor />} />
+              <Route path="/FeedbackOptions" element={<FeedbackOptions />} />
+              <Route path="/InstructorProfile" element={<InstructorProfile />} />
+              <Route path="/AboutUs" element={<AboutUs/>} />
+              <Route path="/ContactUs" element={<ContactUs />} />
+              <Route path="/EditInstructorFeedback/:id" element={<EditInstructorFeedback />} />
 
 
         </Routes>
