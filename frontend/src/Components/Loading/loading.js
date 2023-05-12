93b4c97933b2fdb27033ac9f127f7eb30ef09913
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-05-12 09:54:07 +0530

    s

--- frontend/src/Components/Loading/loading.js
+++ frontend/src/Components/Loading/loading.js
@@ -1,4 +1,26 @@
-import '../Loading/Loading.css'
+<<<<<<< HEAD
+import './Loading.css'
+
+/* JS functions to show and hide the spinner */
+export function showLoadingSpinner() {
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    const spinnerIcon = document.createElement('div');
+    spinnerIcon.classList.add('spinner-icon');
+
+    spinner.appendChild(spinnerIcon);
+    document.body.appendChild(spinner);
+}
+
+export function hideLoadingSpinner() {
+    const spinner = document.querySelector('.spinner');
+    if (spinner) {
+        spinner.remove();
+    }
+}
+=======
+import '../Loading/Loding.css'
 
  
 
@@ -26,3 +48,4 @@ export function hideLoadingSpinner() {
         spinner.remove();
     }
 }
+>>>>>>> origin/Tharani
