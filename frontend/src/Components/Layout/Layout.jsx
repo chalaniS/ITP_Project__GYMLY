8a775ffb2e389a6142fc5aaca35786de9752da83
Tharani Karunatilleke <99175210+Thraranikarunatilleke@users.noreply.github.com>
2023-05-12 21:34:37 +0530

    Layout

---
+++ frontend/src/Components/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../Common/Navbar'
+import Footer from '../Common/Footer'
+import Router from '../../router/Routes'
+
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+            <Router />
+            <Footer />
+            </>
+
+
+    );
+}
+
+export default Layout;
